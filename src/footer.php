2b9e9e2fe74d698829b6f0ccca0c68a7f3c1cb0e
Jon Senterfitt <thetwosents@gmail.com>
2017-06-21 18:23:30 -0500

    Updated general template styles

--- src/footer.php
+++ src/footer.php
@@ -1,18 +1,9 @@
-			<!-- footer -->
 			<footer class="footer" role="contentinfo">
-
-				<!-- copyright -->
 				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org">WordPress</a> &amp; <a href="//html5blank.com">HTML5 Blank</a>.
+					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>
 				</p>
-				<!-- /copyright -->
-
 			</footer>
-			<!-- /footer -->
-
 		</div>
-		<!-- /wrapper -->
 
 		<?php wp_footer(); ?>
 
--- src/index.php
+++ src/index.php
@@ -1,19 +1,10 @@
 <?php get_header(); ?>
 
 	<main role="main" aria-label="Content">
-		<!-- section -->
 		<section>
-
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
-
+			<h1>Welcome to the new world</h1>
 			<?php get_template_part('loop'); ?>
-
-			<?php get_template_part('pagination'); ?>
-
 		</section>
-		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
